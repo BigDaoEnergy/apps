2d6b2f8cbf4c32d2945d548c01908d2593b23964
badmonkey9669 <badmonkey9669@protonmail.com>
2022-01-17 14:44:13 -0600

    rework packages

---
+++ packages/apps/src/App.tsx
@@ -0,0 +1,12 @@
+
+import { Box } from '@chakra-ui/react';
+import React from 'react';
+import logo from './logo.svg';
+
+import { ChakraProvider } from "@chakra-ui/react"
+
+// Do this at the root of your application
+function App() {
+  return <ChakraProvider>hi</ChakraProvider>
+}
+export default App;
