1b6b1440fc93fee2b54cfbd1e69c238e5c09de8b
badmonkey9669 <badmonkey9669@protonmail.com>
2022-01-17 15:38:34 -0600

    fxi: build

--- packages/apps/src/App.tsx
+++ packages/apps/src/App.tsx
@@ -1,12 +1,13 @@
 
 import { Box } from '@chakra-ui/react';
 import React from 'react';
-import logo from './logo.svg';
 
 import { ChakraProvider } from "@chakra-ui/react"
 
 // Do this at the root of your application
 function App() {
-  return <ChakraProvider>hi</ChakraProvider>
+  return <ChakraProvider><Box>
+    hi
+    </Box></ChakraProvider>
 }
 export default App;
