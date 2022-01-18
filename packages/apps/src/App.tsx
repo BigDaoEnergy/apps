cd2a7c1f6c49ab289aa9abd0097017825ef68bab
badmonkey9669 <badmonkey9669@protonmail.com>
2022-01-18 13:33:26 -0600

    fix: router

--- packages/apps/src/App.tsx
+++ packages/apps/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Center, ChakraProvider, Container } from '@chakra-ui/react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { Web3Context, Web3ContextProvider } from './contexts';
 import { theme } from './shared/theme';
@@ -8,19 +9,26 @@ import Landing from './pages/Landing';
 // Do this at the root of your application
 function App() {
   return (
-    <Web3ContextProvider>
-      <ChakraProvider theme={theme}>
-        <Web3Context.Consumer>
-          {() => (
-            <Container>
-              <Center height={'100vh'}>
-                <Landing />
-              </Center>
-            </Container>
-          )}
-        </Web3Context.Consumer>
-      </ChakraProvider>
-    </Web3ContextProvider>
+    <BrowserRouter>
+      <Web3ContextProvider>
+        <ChakraProvider theme={theme}>
+          <Web3Context.Consumer>
+            {() => (
+              <Container>
+                <Center height={'100vh'}>
+                  <Routes>
+                    <Route path='/' element={<Landing />} />
+                    <Route path='/about' element={<Landing />} />
+                    <Route path='/team' element={<Landing />} />
+                    <Route path='/rtfm' element={<Landing />} />
+                  </Routes>
+                </Center>
+              </Container>
+            )}
+          </Web3Context.Consumer>
+        </ChakraProvider>
+      </Web3ContextProvider>
+    </BrowserRouter>
   );
 }
 export default App;
