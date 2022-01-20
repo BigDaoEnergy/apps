979a183d900bd85fa4b186398c6ce9333c03528b
badmonkey9669 <badmonkey9669@protonmail.com>
2022-01-20 16:31:43 -0600

    fix: test setup

--- packages/bde-contracts/test/index.ts
+++ packages/bde-contracts/test/index.ts
@@ -1,15 +1,39 @@
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
 
-describe('Big DAO Energy Token', function () {
-  describe('Whitelist', async function () {
-    it("should accept new whitelist address", async function () {
-      const BDE = await ethers.getContractFactory('BigDaoEnergy');
-      const instance = await BDE.deploy("BigDAOenergy", "BDE", 10000000);
-      await instance.deployed();
+import { BigDaoEnergy } from '../typechain';
 
-      expect(await instance.cap()).to.equal(10000000);
-      expect(await instance.totalSupply()).to.equal(0);
+const CAP = 100000000; // 100 million
+const INITIAL_SUPPLY = 69000000; // 69 million
+
+describe('Big DAO Energy Token', function () {
+  let BDE: BigDaoEnergy;
+  let ALICE: SignerWithAddress;
+  let BOB: SignerWithAddress;
+  let CHARLIE: SignerWithAddress;
+
+  before(async () => {
+      const BDEFactory = await ethers.getContractFactory('BigDaoEnergy');
+      const instance = await BDEFactory.deploy("BigDAOenergy", "BDE", CAP);
+      await instance.deployed();
+      BDE = instance;
+
+      [ALICE, BOB, CHARLIE] = await ethers.getSigners();
+  })
+
+  describe('Whitelist', async function () {
+    it("should have correct initial state", async function () {
+      expect(await BDE.cap()).to.equal(CAP);
+      expect(await BDE.totalSupply()).to.equal(0);
     });
+
+    it('should mint', async function() {
+      expect(await BDE.connect(ALICE).initialMint(INITIAL_SUPPLY)).to.emit(BDE, "Transfer");
+      
+      expect(await BDE.totalSupply()).to.equal(INITIAL_SUPPLY);
+
+      expect(await BDE.balanceOf(ALICE.address)).to.equal(INITIAL_SUPPLY);
+    })
   })
 });
